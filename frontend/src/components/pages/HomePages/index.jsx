3ec3a5b24a5b3439e208a5c855808b13d9228c91
sahmlo <rhana.souza@ba.estudante.senai.br>
2025-10-30 16:40:27 -0300

    salvar

---
+++ frontend/src/components/pages/HomePages/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'; 
+
+function HomePages() {
+  return (
+    <div className="home-container">
+      <h1>Bem-vindo ao sistema de clientes</h1>
+      <p>Gerencie seus clientes de forma eficiente e prática.</p>
+      <p>Utilize o menu para navegar entre as opções disponíveis.</p>
+    </div>
+  );
+}
+
+export default HomePages;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // configuraração da isntalação do axios om a url base da api. 
 const api = axios.create({
   baseURL: 'http://localhost:3000',
+  timeout: 500, 
 });
 
 export default api;
